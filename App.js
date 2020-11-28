2900c37127aca52f2cb5d2f8947e10f4dfdfe129
@mudassirahmad <mudassirahmad998@gmail.com>
2020-11-28 16:55:27 +0500

    Task 6...History functionality is implemented

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, Component, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableWithoutFeedback, Keyboard, Alert, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableWithoutFeedback, Keyboard, Alert, TouchableOpacity, Modal, TouchableHighlight } from 'react-native';
 
 export default function App() {
 
@@ -8,8 +7,12 @@ export default function App() {
   const [finalPrice, setFinalPrice] = useState();
   const [enteredPrice, setEnterdPrice] = useState("");
   const [discount, setDiscount] = useState("");
-  const [record,setRecord]=useState([]);
-  const [index, setIndex]=useState(0);
+  const [record, setRecord] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [displayfp, setDisplayfp] = useState("");
+  const [displayD, setDisplayD] = useState("");
+  const [displayOp, serDisplayOp] = useState("");
 
   function Calculate() {
     var disc = parseFloat(discount);
@@ -27,12 +30,42 @@ export default function App() {
     }
   }
 
-  function saveRecord(){
-    
-    var finalP=finalPrice.toString();
-    var data=("Orginal Price: "+ enteredPrice+ "   Discount:"+discount+ "   Final Price:"+ finalP).toString();
-    setRecord(record[index]=data);
-    setIndex(index+1);
+  function saveRecord() {
+
+    var finalP = finalPrice.toString();
+    var op = parseFloat(enteredPrice);
+    setRecord((record) => [...record, enteredPrice]);
+    setRecord((record) => [...record, discount]);
+    setRecord((record) => [...record, finalP]);
+
+    console.log(record);
+    setIndex(index + 1);
+  }
+  function setDisplay() {
+
+    var valuesfp = "";
+    var valuesOp = "";
+    var valuesDp = "";
+    var check = 0;
+    for (let i = 0; i < record.length; i++) {
+      if (check === 0) {
+        valuesOp += record[i] + "\n";
+      }
+      if (check === 1) {
+        valuesDp += record[i] + "\n"
+      }
+      if (check === 2) {
+        valuesfp += record[i] + "\n"
+      }
+      check++;
+      if (check == 3) {
+        check = 0;
+      }
+    }
+
+    setDisplayfp(valuesfp);
+    setDisplayD(valuesDp);
+    serDisplayOp(valuesOp);
   }
 
   return (
@@ -57,20 +90,42 @@ export default function App() {
             <Text style={[styles.textStyle, { fontSize: 30 }]}>Final Price </Text>
             <Text style={styles.resultText}>{enteredPrice === "" || discount === "" ? "--" : finalPrice}</Text>
           </View>
-          <View style={{flexDirection:"row", justifyContent:"space-between", alignItems:"center", marginTop:15}}>
-            <TouchableOpacity onPress={()=>saveRecord()} style={styles.buttonStyle}> 
+          <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginTop: "10%" }}>
+            <TouchableOpacity onPress={() => saveRecord()} style={styles.buttonStyle}>
               <Text style={styles.buttonTextStyle}>Save</Text>
             </TouchableOpacity>
-            <View style={styles.buttonStyle}>
-              <Text style={styles.buttonTextStyle}>History</Text>
-            </View>
-            
+
+            <TouchableOpacity style={styles.buttonStyle} onPress={() => { setModalVisible(true); setDisplay(); }}>
+              <View>
+                <Modal animationType="slide" transparent={true} visible={modalVisible}>
+                  <View style={styles.centeredView}>
+                    <View style={[styles.modalView]}>
+                      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                        <Text style={{ fontSize: 15, fontWeight: "bold" }}>Original Price</Text>
+                        <Text style={{ fontSize: 15, fontWeight: "bold" }}>Discount</Text>
+                        <Text style={{ fontSize: 15, fontWeight: "bold" }}>Final Price</Text>
+                      </View>
+                      <View style={{ flexDirection: "row", justifyContent: "space-between", marginTop: 10 }}>
+                        <Text style={{ fontSize: 15, }}>{displayOp}</Text>
+                        <Text style={{ fontSize: 15, }}>{displayD}</Text>
+                        <Text style={{ fontSize: 15, }}>{displayfp}</Text>
+                      </View>
+                      <TouchableHighlight style={styles.openButton} onPress={() => { setModalVisible(!modalVisible); }} >
+                        <Text style={styles.MbtextStyle}>Hide History</Text>
+                      </TouchableHighlight>
+                    </View>
+                  </View>
+                </Modal>
+                <Text style={styles.buttonTextStyle} >History</Text>
+              </View>
+            </TouchableOpacity>
+
+          </View>
+
         </View>
-        
-        </View>
-        
+
       </TouchableWithoutFeedback>
-      
+
     </View>
   );
 }
@@ -96,6 +151,29 @@ const styles = StyleSheet.create({
     width: "80%", height: 40, borderColor: "#05375a", borderWidth: 2, borderTopWidth: 0, borderLeftColor: 0, borderRightWidth: 0, marginBottom: 20, fontSize: 20, marginTop: 4
   },
   resultText: { fontSize: 25, textAlign: "center", marginTop: 10, marginBottom: 10, fontWeight: "bold" },
-  buttonStyle: {backgroundColor:"#01877C", width:"40%", height:60, justifyContent:"center", alignItems:"center", borderRadius:15},
-  buttonTextStyle: {color:"white", fontSize:24, fontWeight:"bold"}
+  buttonStyle: {
+    backgroundColor: "#01877C", width: "40%", height: 60, justifyContent: "center", alignItems: "center", borderRadius: 15
+  },
+  buttonTextStyle: {
+    color: "white", fontSize: 24, fontWeight: "bold"
+  },
+  centeredView: {
+    flex: 1, justifyContent: "center", alignItems: "center", marginTop: 22
+  },
+  modalView: {
+    width: "90%", margin: 20, backgroundColor: "white", borderRadius: 20, padding: 35, shadowColor: "#000", shadowOffset: {
+      width: 0, height: 2
+    }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5
+  },
+  openButton: {
+    backgroundColor: "#2196F3", borderRadius: 20, padding: 10, elevation: 2, marginTop: 5
+  },
+  modalText: {
+    marginBottom: 15, textAlign: "justify", alignContent: "space-between", justifyContent: "space-evenly"
+  },
+
+  MbtextStyle: {
+    color: "white", fontWeight: "bold", textAlign: "center"
+  },
+
 });
